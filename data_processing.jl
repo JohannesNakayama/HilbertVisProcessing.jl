bd990ff1757c840e35cbde8b2c2824ebe687f9fa
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2021-03-12 16:55:11 +0100

    implemented less ram-intensive processing

--- data_processing.jl
+++ data_processing.jl
@@ -1,8 +1,10 @@
 using JSON
 using DataFrames
 using Query
-using Feather
+using CSV
+using Suppressor
 FLAG = false
+BATCHSIZE = 10
 pattern = ".7z"
 data_path = "data"
 
@@ -44,6 +46,121 @@ end
 # test if all elements in a list are equal [FIND REFERENCE]
 allequal(x) = all(y -> y == x[1], x)
 
+
+function read_and_split_raw_data(jsonpath) 
+    # seperate metadata and search results
+    metadata = read_json_sample(jsonpath)
+    results = pop!(metadata)
+    return metadata, results
+end
+
+
+function metadata_to_df(metadata) 
+    metadata_df_list = DataFrame[]
+    for i in 1:length(metadata)
+        tmp_df_list = DataFrame[]
+        for j in 1:length(metadata[i])
+            push!(tmp_df_list, DataFrame(metadata[i][j]))
+        end
+        tmp_df = reduce(vcat, tmp_df_list)
+        push!(metadata_df_list, tmp_df)
+    end
+    metadata_df = reduce(vcat, metadata_df_list)
+    return metadata_df
+end
+
+function results_to_df(results)
+    results_df_list = DataFrame[]
+    for result_list in results
+        tmp_url_list = [i["sourceUrl"] for i in result_list["result"]]
+        # skip faulty result lists
+        if allequal(tmp_url_list)
+            continue
+        end
+        tmp_df = DataFrame(sourceUrl = tmp_url_list)
+        tmp_df[:, :result_hash] .= result_list["result_hash"]
+        tmp_df[:, :rank] = 1:length(tmp_url_list)
+        push!(results_df_list, tmp_df)
+    end
+    results_df = reduce(vcat, results_df_list)
+    return results_df
+end
+
+function reduce_dataset(df)
+    df = df |> @filter(_.search_type == "search") |> DataFrame
+    select!(df, Not([:search_type, :plugin_id]))
+    return df
+end
+
+
+
+##### ----------------------------- #####
+##### RAW DATA PROCESSING PROCEDURE #####
+##### ----------------------------- #####
+
+if !("processed" in readdir()) 
+    mkdir("processed")
+end
+
+file_endings = Dict(
+    "7z" => ".7z",
+    "json" => ".json"
+)
+data_path = "data"
+archive_list = [i for i in readdir(data_path) if occursin(file_endings["7z"], i)]
+filenames_map = Dict(
+    "Alice Weidel" => "aliceweidel",
+    "CSU" => "csu",
+    "Christian Lindner" => "christianlindner",
+    "Cem Özdemir" => "cemoezdemir",
+    "Dietmar Bartsch" => "dietmarbartsch",
+    "SPD" => "spd",
+    "FDP" => "fdp",
+    "CDU" => "cdu",
+    "Bündnis90/Die Grünen" => "gruene",
+    "Alexander Gauland" => "alexandergauland",
+    "Angela Merkel" => "angelamerkel",
+    "Martin Schulz" => "martinschulz",
+    "AfD" => "afd",
+    "Katrin Göring-Eckardt" => "katringoeringeckardt",
+    "Die Linke" => "linke",
+    "Sahra Wagenknecht" => "sahrawagenknecht"
+)
+keywords = collect(keys(filenames_map))
+
+for archive in archive_list
+    # read and seperate metadata and search results
+    extract_json(data_path, archive)
+    jsonpath = joinpath.(data_path, [i for i in readdir(data_path) if occursin(file_endings["json"], i)])[1]
+    @suppress metadata, results = read_and_split_raw_data(jsonpath);
+    remove_json(data_path)
+
+    # put everything into one dataframe
+    metadata = metadata_to_df(metadata)
+    results = results_to_df(results)
+    day_df = innerjoin(results, metadata, on = :result_hash)
+    day_df = reduce_dataset(day_df)
+
+    # create datasets by keyword
+    for k in keywords
+        filename = filenames_map[k] * ".csv"
+        keyword_df = day_df |> @filter(_.keyword == k) |> DataFrame
+        if filename in readdir("processed")
+            CSV.write(joinpath("processed", filename), keyword_df)
+        else
+            CSV.write(joinpath("processed", filename), keyword_df, append = true)
+        end
+    end
+end
+
+
+
+
+
+##### ----------- #####
+##### OTHER STUFF #####
+##### ----------- #####
+
 # quick and dirty tests
 if FLAG 
     begin  
@@ -83,106 +200,3 @@ end
 # yet another data issue: &sa...&ved -> some urls with google specific paths seem broken
 # fix broken urls (or don't break them in the first place)
 # remove "sa=.*"
-
-
-##### ----------------------------- #####
-##### RAW DATA PROCESSING PROCEDURE #####
-##### ----------------------------- #####
-
-if !("processed" in readdir()) 
-    mkdir("processed")
-end
-
-file_endings = Dict(
-    "7z" => ".7z",
-    "json" => ".json"
-)
-data_path = "data"
-archive_list = [i for i in readdir(data_path) if occursin(file_endings["7z"], i)]
-full_data_set = DataFrame[]
-for archive in archive_list
-    extract_json(data_path, archive)
-    json_list = joinpath.(data_path, [i for i in readdir(data_path) if occursin(file_endings["json"], i)])
-
-    # seperate meta data and search results
-    meta_data = read_json_sample(json_list[1]);
-    result_data = pop!(meta_data);
-
-    # convert meta data into dataframe
-    meta_data_df_list = DataFrame[]
-    for i in 1:length(meta_data)
-        tmp_df_list = DataFrame[]
-        for j in 1:length(meta_data[i])
-            push!(tmp_df_list, DataFrame(meta_data[i][j]))
-        end
-        tmp_df = reduce(vcat, tmp_df_list)
-        push!(meta_data_df_list, tmp_df)
-    end
-    meta_data_df = reduce(vcat, meta_data_df_list)
-
-    # convert search results to dataframe
-    result_data_df_list = DataFrame[]
-    for result_list in result_data
-        # tmp_result_item = result_data[1]  # 1 becomes i
-        tmp_url_list = [i["sourceUrl"] for i in result_list["result"]]
-        # skip faulty result lists
-        if allequal(tmp_url_list)
-            continue
-        end
-        tmp_df = DataFrame(sourceUrl = tmp_url_list)
-        tmp_df[:, :result_hash] .= result_list["result_hash"];
-        tmp_df[:, :rank] = 1:length(tmp_url_list);
-        push!(result_data_df_list, tmp_df)
-    end
-    result_data_df = reduce(vcat, result_data_df_list)
-    
-    # join meta data to result lists
-    day_df = innerjoin(result_data_df, meta_data_df, on = :result_hash)
-
-    # reduce data set to search (exclude news)
-    day_df = day_df |> @filter(_.search_type == "search") |> DataFrame
-    
-    # remove redundant columns
-    select!(day_df, Not([:search_type, :plugin_id]))
-
-    # add data set for this day to the full data set and clean up
-    push!(full_data_set, day_df)
-    remove_json(data_path)
-end
-
-# get all search keywords and assign file names
-keywords = unique(full_data_set[1][!, :keyword])
-filenames_map = Dict(
-    "Alice Weidel" => "aliceweidel",
-    "CSU" => "csu",
-    "Christian Lindner" => "christianlindner",
-    "Cem Özdemir" => "cemoezdemir",
-    "Dietmar Bartsch" => "dietmarbartsch",
-    "SPD" => "spd",
-    "FDP" => "fdp",
-    "CDU" => "cdu",
-    "Bündnis90/Die Grünen" => "gruene",
-    "Alexander Gauland" => "alexandergauland",
-    "Angela Merkel" => "angelamerkel",
-    "Martin Schulz" => "martinschulz",
-    "AfD" => "afd",
-    "Katrin Göring-Eckardt" => "katringoeringeckardt",
-    "Die Linke" => "linke",
-    "Sahra Wagenknecht" => "sahrawagenknecht"
-)
-
-# create datasets by keyword
-for k in keywords
-
-    keyword_df_list = DataFrame[]
-    for d in full_data_set
-        tmp = d |> @filter(_.keyword == k) |> DataFrame
-        push!(keyword_df_list, tmp)
-    end
-    keyword_df = reduce(vcat, keyword_df_list)
-    Feather.write(joinpath("processed", filenames_map[k] * ".feather"), keyword_df)    
-
-end
-
-
-
